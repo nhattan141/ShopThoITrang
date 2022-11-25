8a45df8fc9b9b547f2f588637496cda467b0be01
nhattan141 <trngtn44@gmail.com>
2022-11-25 15:34:09 +0700

    add address column in order admin

--- src/pages/components-overview/Orders.js
+++ src/pages/components-overview/Orders.js
@@ -68,6 +68,7 @@ const ComponentOrders = () => {
                         <TableRow>
                             <TableCell>Mã đơn hàng</TableCell>
                             <TableCell align="right">Ngày đặt</TableCell>
+                            <TableCell align="right">Địa chỉ</TableCell>
                             <TableCell align="right">Tổng đơn</TableCell>
                             <TableCell align="right">Trạng thái</TableCell>
                         </TableRow>
@@ -84,6 +85,7 @@ const ComponentOrders = () => {
                                     {row._id}
                                 </TableCell>
                                 <TableCell align="right">{row.Date}</TableCell>
+                                <TableCell align="right">{row.Address}</TableCell>
                                 <TableCell align="right">{new Intl.NumberFormat().format(row.Total)}&nbsp;VNĐ</TableCell>
                                 <TableCell align="right">{row.Status}</TableCell>
                             </TableRow>
